5d10f379c155504bb6c4a194df7867ddd457f06a
Thomas Gielfeldt <thomas@gielfeldt.dk>
2015-08-08 15:39:37 +0200

    Add example of keyed shutdown.

---
+++ examples/keyed_shutdown.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Gielfeldt\Example;
+
+require 'vendor/autoload.php';
+
+use Gielfeldt\ShutdownHandler;
+
+/**
+ * Simple shutdown handler callback.
+ *
+ * @param string $message
+ *   Message to display during shutdown.
+ */
+function myshutdownhandler($message = '')
+{
+    echo "Goodbye $message\n";
+}
+
+// Register shutdown handler to be run during PHP shutdown phase.
+$handlers = array();
+$handlers[] = new ShutdownHandler('\Gielfeldt\Example\myshutdownhandler', array('test0'));
+$handlers[] = new ShutdownHandler('\Gielfeldt\Example\myshutdownhandler', array('test1'), 'key1');
+$handlers[] = new ShutdownHandler('\Gielfeldt\Example\myshutdownhandler', array('test2'), 'key1');
+$handlers[] = new ShutdownHandler('\Gielfeldt\Example\myshutdownhandler', array('test3'), 'key1');
+$handlers[] = new ShutdownHandler('\Gielfeldt\Example\myshutdownhandler', array('test4'), 'key1');
+
+echo "Hello world\n";
+
+// This has no effect.
+unset($handlers[0], $handlers[1], $handlers[2], $handlers[3]);
+
+// This has no effect.
+unset($handlers[3], $handlers[2], $handlers[1], $handlers[0]);
+
+echo "All local objects unset\n";
+
+// 'test0' and 'test4' will now be run.
